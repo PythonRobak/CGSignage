bac3e6429be9368d4f24a2757004e126e55c30f9
Adrian Jarocki <adrian@cgmasters.pl>
2018-12-06 23:44:20 +0100

    database drop 002 przed dopisaniem kolejnych modeli

--- webadminpanel/models.py
+++ webadminpanel/models.py
@@ -17,3 +17,21 @@ class Media(models.Model):
 
     def filename(self):
         return os.path.basename(self.file.name)
+
+
+# class Group(models.Model):
+#     name = models.CharField(max_length=256)
+#     description = models.TextField()
+#     date_added = models.DateTimeField(auto_now_add=True, blank=True)
+#     last_edited = models.DateTimeField(auto_now=True, blank=True)
+#     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f"{self.name}"
+
+
+
+# class Campaign(models.Model):
+#     name = models.CharField(max_length=256)
+#     media = models.ForeignKey(Media, on_delete=models.CASCADE, blank=True)
+#     display_unit = models.ForeignKey(DisplayUnit, on_delete=models.CASCADE, blank=True)
